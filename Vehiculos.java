5475cb8fc131f785e712d5d15d2224b699b133ec
Sebastian Congo <96399138+Einarr07@users.noreply.github.com>
2022-11-26 12:35:44 -0500

    Segunda  version del proyecto

--- Vehiculos.java
+++ Vehiculos.java
@@ -14,6 +14,7 @@ public class Vehiculos {
     Scanner entrada = new Scanner(System.in);
     //Constructor
     public Vehiculos (){
+        tipo = "";
         marca = "";
         modelo = "";
         color = "";
@@ -26,7 +27,7 @@ public class Vehiculos {
     //Menu de opciones
     int menu(){
         int op = -1;
-        while ((op < 0) && (op > 3)) {
+        while ((op < 0) || (op > 3)) {
             System.out.println("---------------------------------------");
             System.out.println("| BIENVENIDO AL REGISTRO DE VEHICULOS |");
             System.out.println("---------------------------------------");
@@ -39,7 +40,8 @@ public class Vehiculos {
             System.out.println("---------------------------------------------");
             System.out.print("Ingrese la opcion que desea ejecutar: ");
             op = entrada.nextInt();
-                if((op < 0) && (op > 3)){
+
+                if((op < 0) || (op > 3)){
                     System.out.println("--------------------");
                     System.out.println("| Opcion no valida |");
                     System.out.println("--------------------");
@@ -50,17 +52,18 @@ public class Vehiculos {
     //menu cantidad de vehiculos
     int menu_cantidad(){
         int op = -1;
-        while((op < 0) && (op > 5)){
+        while((op < 0) || (op > 5)){
             System.out.println("---------------------------------------------");
-            System.out.println("|  ¿Cuantos vehículos desea ingresar?       |");
             System.out.println("|  NOTA: La cantidad maxima es 5 vehiculos  |");
             System.out.println("---------------------------------------------");
+            System.out.print("¿Cuantos vehículos desea ingresar? ");
             op = entrada.nextInt();
-        }
-        if((op < 0) && (op > 5)){
-            System.out.println("--------------------");
-            System.out.println("| Opcion no valida |");
-            System.out.println("--------------------");
+
+            if((op < 0) || (op > 5)){
+                System.out.println("--------------------");
+                System.out.println("| Opcion no valida |");
+                System.out.println("--------------------");
+            }
         }
         return op;
     }
@@ -70,6 +73,173 @@ public class Vehiculos {
         datos1.setTipo(entrada.nextLine());
         System.out.println("Ingrese la marca del vehiculo");
         datos1.setMarca(entrada.nextLine());
+        System.out.println("Ingrese el modelo del vehiculo");
+        datos1.setModelo(entrada.nextLine());
+        System.out.println("Ingrese el color del vehiculo");
+        datos1.setColor(entrada.nextLine());
+        System.out.println("Ingrese las placas del vehiculo");
+        datos1.setPlacas(entrada.nextLine());
+        System.out.println("Ingrese comodidad del vehiculo");
+        datos1.setComodidad(entrada.nextLine());
+        System.out.println("Ingrese el costo del mantenimiento del vehiculo");
+        datos1.setCoste_matenimiento(entrada.nextLine());
+        System.out.println("Ingrese el nivel de seguridad del vehiculo");
+        datos1.setSeguridad(entrada.nextLine());
+        System.out.println("Ingrese el año de fabricacion del vehiculo");
+        datos1.setAnio_fabricacion(entrada.nextInt());
+    }
+    public void car2 (Vehiculos datos2){
+        System.out.println("Ingrese el tipo de vehiculo que va a registrar");
+        datos2.setTipo(entrada.nextLine());
+        System.out.println("Ingrese la marca del vehiculo");
+        datos2.setMarca(entrada.nextLine());
+        System.out.println("Ingrese el modelo del vehiculo");
+        datos2.setModelo(entrada.nextLine());
+        System.out.println("Ingrese el color del vehiculo");
+        datos2.setColor(entrada.nextLine());
+        System.out.println("Ingrese las placas del vehiculo");
+        datos2.setPlacas(entrada.nextLine());
+        System.out.println("Ingrese comodidad del vehiculo");
+        datos2.setComodidad(entrada.nextLine());
+        System.out.println("Ingrese el costo del mantenimiento del vehiculo");
+        datos2.setCoste_matenimiento(entrada.nextLine());
+        System.out.println("Ingrese el nivel de seguridad del vehiculo");
+        datos2.setSeguridad(entrada.nextLine());
+        System.out.println("Ingrese el año de fabricacion del vehiculo");
+        datos2.setAnio_fabricacion(entrada.nextInt());
+    }
+    public void car3 (Vehiculos datos3){
+        System.out.println("Ingrese el tipo de vehiculo que va a registrar");
+        datos3.setTipo(entrada.nextLine());
+        System.out.println("Ingrese la marca del vehiculo");
+        datos3.setMarca(entrada.nextLine());
+        System.out.println("Ingrese el modelo del vehiculo");
+        datos3.setModelo(entrada.nextLine());
+        System.out.println("Ingrese el color del vehiculo");
+        datos3.setColor(entrada.nextLine());
+        System.out.println("Ingrese las placas del vehiculo");
+        datos3.setPlacas(entrada.nextLine());
+        System.out.println("Ingrese comodidad del vehiculo");
+        datos3.setComodidad(entrada.nextLine());
+        System.out.println("Ingrese el costo del mantenimiento del vehiculo");
+        datos3.setCoste_matenimiento(entrada.nextLine());
+        System.out.println("Ingrese el nivel de seguridad del vehiculo");
+        datos3.setSeguridad(entrada.nextLine());
+        System.out.println("Ingrese el año de fabricacion del vehiculo");
+        datos3.setAnio_fabricacion(entrada.nextInt());
+    }
+    public void car4 (Vehiculos datos4){
+        System.out.println("Ingrese el tipo de vehiculo que va a registrar");
+        datos4.setTipo(entrada.nextLine());
+        System.out.println("Ingrese la marca del vehiculo");
+        datos4.setMarca(entrada.nextLine());
+        System.out.println("Ingrese el modelo del vehiculo");
+        datos4.setModelo(entrada.nextLine());
+        System.out.println("Ingrese el color del vehiculo");
+        datos4.setColor(entrada.nextLine());
+        System.out.println("Ingrese las placas del vehiculo");
+        datos4.setPlacas(entrada.nextLine());
+        System.out.println("Ingrese comodidad del vehiculo");
+        datos4.setComodidad(entrada.nextLine());
+        System.out.println("Ingrese el costo del mantenimiento del vehiculo");
+        datos4.setCoste_matenimiento(entrada.nextLine());
+        System.out.println("Ingrese el nivel de seguridad del vehiculo");
+        datos4.setSeguridad(entrada.nextLine());
+        System.out.println("Ingrese el año de fabricacion del vehiculo");
+        datos4.setAnio_fabricacion(entrada.nextInt());
+    }
+    public void car5 (Vehiculos datos5){
+        System.out.println("Ingrese el tipo de vehiculo que va a registrar");
+        datos5.setTipo(entrada.nextLine());
+        System.out.println("Ingrese la marca del vehiculo");
+        datos5.setMarca(entrada.nextLine());
+        System.out.println("Ingrese el modelo del vehiculo");
+        datos5.setModelo(entrada.nextLine());
+        System.out.println("Ingrese el color del vehiculo");
+        datos5.setColor(entrada.nextLine());
+        System.out.println("Ingrese las placas del vehiculo");
+        datos5.setPlacas(entrada.nextLine());
+        System.out.println("Ingrese comodidad del vehiculo");
+        datos5.setComodidad(entrada.nextLine());
+        System.out.println("Ingrese el costo del mantenimiento del vehiculo");
+        datos5.setCoste_matenimiento(entrada.nextLine());
+        System.out.println("Ingrese el nivel de seguridad del vehiculo");
+        datos5.setSeguridad(entrada.nextLine());
+        System.out.println("Ingrese el año de fabricacion del vehiculo");
+        datos5.setAnio_fabricacion(entrada.nextInt());
+    }
+    public void impresion (Vehiculos datos1, Vehiculos datos2, Vehiculos datos3, Vehiculos datos4, Vehiculos datos5, int op_registro){
+
+        if(op_registro >= 1) {
+            System.out.println("--------------------------");
+            System.out.println("| Datos del 1er vehiculo |");
+            System.out.println("--------------------------");
+            System.out.println("Tipo: " + datos1.getTipo());
+            System.out.println("Marca: " + datos1.getMarca());
+            System.out.println("Modelo: " + datos1.getModelo());
+            System.out.println("Color: " + datos1.getColor());
+            System.out.println("Placas: " + datos1.getPlacas());
+            System.out.println("Comodidad: " + datos1.getComodidad());
+            System.out.println("Costo Mantenimiento: " + datos1.getCoste_matenimiento());
+            System.out.println("Nivel de seguridad: " + datos1.getSeguridad());
+            System.out.println("Año de fabricacion: " + datos1.getAnio_fabricacion());
+        }
+        if(op_registro >= 2) {
+            System.out.println("--------------------------");
+            System.out.println("| Datos del 2do vehiculo |");
+            System.out.println("--------------------------");
+            System.out.println("Tipo: " + datos2.getTipo());
+            System.out.println("Marca: " + datos2.getMarca());
+            System.out.println("Modelo: " + datos2.getModelo());
+            System.out.println("Color: " + datos2.getColor());
+            System.out.println("Placas: " + datos2.getPlacas());
+            System.out.println("Comodidad: " + datos2.getComodidad());
+            System.out.println("Costo Mantenimiento: " + datos2.getCoste_matenimiento());
+            System.out.println("Nivel de seguridad: " + datos2.getSeguridad());
+            System.out.println("Año de fabricacion: " + datos2.getAnio_fabricacion());
+        }
+        if(op_registro >= 3) {
+            System.out.println("--------------------------");
+            System.out.println("| Datos del 3er vehiculo |");
+            System.out.println("--------------------------");
+            System.out.println("Tipo: " + datos3.getTipo());
+            System.out.println("Marca: " + datos3.getMarca());
+            System.out.println("Modelo: " + datos3.getModelo());
+            System.out.println("Color: " + datos3.getColor());
+            System.out.println("Placas: " + datos3.getPlacas());
+            System.out.println("Comodidad: " + datos3.getComodidad());
+            System.out.println("Costo Mantenimiento: " + datos3.getCoste_matenimiento());
+            System.out.println("Nivel de seguridad: " + datos3.getSeguridad());
+            System.out.println("Año de fabricacion: " + datos3.getAnio_fabricacion());
+        }
+        if(op_registro >= 4) {
+            System.out.println("--------------------------");
+            System.out.println("| Datos del 4to vehiculo |");
+            System.out.println("--------------------------");
+            System.out.println("Tipo: " + datos4.getTipo());
+            System.out.println("Marca: " + datos4.getMarca());
+            System.out.println("Modelo: " + datos4.getModelo());
+            System.out.println("Color: " + datos4.getColor());
+            System.out.println("Placas: " + datos4.getPlacas());
+            System.out.println("Comodidad: " + datos4.getComodidad());
+            System.out.println("Costo Mantenimiento: " + datos4.getCoste_matenimiento());
+            System.out.println("Nivel de seguridad: " + datos4.getSeguridad());
+            System.out.println("Año de fabricacion: " + datos4.getAnio_fabricacion());
+        }
+        if(op_registro >= 5) {
+            System.out.println("--------------------------");
+            System.out.println("| Datos del 5to vehiculo |");
+            System.out.println("--------------------------");
+            System.out.println("Tipo: " + datos5.getTipo());
+            System.out.println("Marca: " + datos5.getMarca());
+            System.out.println("Modelo: " + datos5.getModelo());
+            System.out.println("Color: " + datos5.getColor());
+            System.out.println("Placas: " + datos5.getPlacas());
+            System.out.println("Comodidad: " + datos5.getComodidad());
+            System.out.println("Costo Mantenimiento: " + datos5.getCoste_matenimiento());
+            System.out.println("Nivel de seguridad: " + datos5.getSeguridad());
+            System.out.println("Año de fabricacion: " + datos5.getAnio_fabricacion());
+        }
     }
     //SETTER AND GETTER
 
--- main.java
+++ main.java
@@ -1,23 +1,61 @@
 public class main {
     public static void main(String[] args){
+        Vehiculos registros = new Vehiculos();
         Vehiculos vehiculo1 = new Vehiculos();
-
+        Vehiculos vehiculo2 = new Vehiculos();
+        Vehiculos vehiculo3 = new Vehiculos();
+        Vehiculos vehiculo4 = new Vehiculos();
+        Vehiculos vehiculo5 = new Vehiculos();
         //
         Vehiculos menu_registro = new Vehiculos();
         Vehiculos menu_de_opciones = new Vehiculos();
+        //
         int op_registro = 0;
         boolean salir = false;
-
-        while (!salir){
+        //
+        while(!salir){
             switch(menu_de_opciones.menu()){
                 case 1:
                     op_registro = menu_registro.menu_cantidad();
+                    System.out.println("");
                     System.out.println("REGISTRE SUS VECHICULOS A CONTINUACION");
-                    if(op_registro == 1){
+                    System.out.println("");
+                    if(op_registro >= 1){
+                        System.out.println("-----------------------------");
+                        System.out.println("| Registro del 1er vehiculo |");
+                        System.out.println("-----------------------------");
                         vehiculo1.car1(vehiculo1);
+                    }
+                    if(op_registro >= 2){
+                        System.out.println("-----------------------------");
+                        System.out.println("| Registro del 2do vehiculo |");
+                        System.out.println("-----------------------------");
+                        vehiculo2.car2(vehiculo2);
+                    }
+                    if(op_registro == 3){
+                        System.out.println("-----------------------------");
+                        System.out.println("| Registro del 3er vehiculo |");
+                        System.out.println("-----------------------------");
+                        vehiculo3.car3(vehiculo3);
+                    }
+                    if(op_registro == 4){
+                        System.out.println("-----------------------------");
+                        System.out.println("| Registro del 4to vehiculo |");
+                        System.out.println("-----------------------------");
+                        vehiculo4.car4(vehiculo4);
+                    }
+                    if(op_registro == 5){
+                        System.out.println("-----------------------------");
+                        System.out.println("| Registro del 5to vehiculo |");
+                        System.out.println("-----------------------------");
+                        vehiculo5.car5(vehiculo5);
                     }
                     break;
                 case 2:
+                    System.out.println("---------------------------------------");
+                    System.out.println("| VEHICULOS REGISTRADOS EN EL SISTEMA |");
+                    System.out.println("---------------------------------------");
+                    registros.impresion(vehiculo1,vehiculo2,vehiculo3,vehiculo4,vehiculo5,op_registro);
                     break;
                 case 3:
                     salir = true;
